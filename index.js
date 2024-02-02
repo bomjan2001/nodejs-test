e05667d51f65e5f3011dd6a26d897c99dc4e4b80
bomjan2001 <155144940+bomjan2001@users.noreply.github.com>
2024-02-02 12:54:59 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const http = require("http");
+const port = process.env.PORT;
+const fs = require("fs");
+
+
+const handle = (fileName, statusCode, req, res) => {
+  fs.readFile(fileName, "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.writeHead(200, {
+        "content-type": "text/html"
+      });
+      res.write(data);
+      res.end();
+    }
+  });
+};
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    handle("index.html", 200, req, res);
+  } else if (req.url === "/about") {
+    handle("about.html", 200, req, res);
+  } else if (req.url === "/contact") {
+    handle("contact.html", 200, req, res);
+  }else if (req.url === "/service") {
+    handle("service.html", 200, req, res);
+  } else {
+    handle("error.html", 200, req, res);
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server is running in`);
+});
